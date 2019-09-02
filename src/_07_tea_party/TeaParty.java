8293eb73d0091b238668911dea505be3c1ff9d0a
League Student <league@iMac-16.attlocal.net>
2019-09-01 17:26:18 -0700

    TeaParty
    :

--- src/_07_tea_party/TeaParty.java
+++ src/_07_tea_party/TeaParty.java
@@ -1,5 +1,50 @@
 package _07_tea_party;
 
+import static org.junit.Assert.assertEquals;
+
+import javax.swing.JOptionPane;
+
+import org.junit.Test;
+
 public class TeaParty {
+
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+
+		if (isKnighted == false) {
+
+			if (isWoman == true) {
+				return "Hello " + "Ms. " + name;
+			}
+			
+		}
+
+		if (isKnighted == false) {
+
+			if (isWoman == false) {
+				return "Hello " + "Mr. " + name;
+			}
+			
+		}
+
+		if (isKnighted == true) {
+
+			if (isWoman == false) {
+				return "Hello " + "Sir " + name;	
+			}
+			
+		}
+
+		if (isKnighted == true) {
+
+			if (isWoman == true) {
+				return "Hello " + "Lady " + name;
+			}
+			
+		}
+
+		// TODO Auto-generated method stub
+		return null;
+
+	}
 
 }
